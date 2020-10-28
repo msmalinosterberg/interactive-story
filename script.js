4b11299f6e895646caa64ef3684f456b6abc4d2b
Malin Österberg <malinosterberg@Malins-MacBook-Air.local>
2020-10-28 15:15:09 +0100

    changed scenes and did a alert

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 let currentScene = 0
 const scenes = [
     {
-        description: "Du befinner dig i vardagsrummet, du ser en trasig stol och två dörrar. Vill du öppna dörren eller sätta dig på stolen?",
-        choices:["Sitt på stolen", "Öppna dörren"], 
-        nextScene: [2, 1]
+        //Scene 0 
+        description: "Har du nyckeln till ytterdörren?",
+        choices:["Ja", "Nej"], 
+        nextScene: [1, 0] // Ja, vidare till scene 1. Nej, fråga din partner om nyckeln. 
 
     }, 
-    {
-        description: "Du står utanför huset och det är superkallt. solen är på väg att gå upp. Vill du titta på soluppgången?",
+    {   //Scene 1 
+        description: "Toppen! Kan du gå till dörren och öppna?",
         choices: ["Ja", "Nej"], 
-        nextScene: [2, 0]
-
-    }
+        nextScene: [2, 0] // Ja, öppnar dörren. Nej, tillbaka till scene 0. 
+    }, 
+    
+ 
 ]; 
 
 //Run our application 
@@ -29,11 +31,14 @@ function handleUserChoice(answer) {
     console.log(answer)
 
     if(answer === scenes[currentScene].choices[0]) {
-        currentScene = scenes[currentScene].nextScene [0] 
+        currentScene = scenes[currentScene].nextScene [0]  
+    } else {
+        alert ('fråga din partner!')
     }
+    
+
     if(answer === scenes[currentScene].choices[1]) {
         currentScene = scenes[currentScene].nextScene [1] 
-    }
-
+ }
     presentScene(); 
 }
