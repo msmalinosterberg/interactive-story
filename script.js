d2f9e3aa85bfad8d5c33c4dc2b7833153e7121f4
Malin Österberg <malinosterberg@Malins-MacBook-Air.local>
2020-10-29 14:38:49 +0100

    Removed button

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 //Define the state for our application 
 let currentScene = 0
 const scenes = [
-    {   //Scene 0 
-        description: "Det är måndag morgon, matlådan är packad och klar. Det är dags att åka till jobbet.Har du nycklarna till ytterdörren?",
+    {   //Scene 0
+        description: "Det är en grå och trist måndag morgon, matlådan är packad och klar. Det är dags att åka till jobbet.Har du nycklarna till ytterdörren?",
         choices:["Ja", "Nej"], 
-        nextScene: [1, 0] // Ja, vidare till scene 1. Nej, alert - kolla i lådan! 
+        nextScene: [1, 7]  
     }, 
-    {   //Scene 1 
+    {   //Scene 1
         description: "Låser du dörren?",
         choices: ["Ja", "Nej"], 
-        nextScene: [2, 1] // Ja, öppnar dörren. Nej, behöver en alert här! Fråga David! 
+        nextScene: [2, 8] 
     }, 
-    {   //Scene 2 
-        description: " Toppen, hoppa in i bilen så kör vi! Du inser att det behövs tankas. Vill du åka till St1, Circle K eller Shell",
-        choices: ["St1", "Circle K", "Shell"], 
-        nextScene: [, 2]
-        
+    {   //Scene 2
+        description: " Toppen, hoppa in i bilen så kör vi! Du inser att det behövs tankas. Vill du åka till St1 eller Circle K?",
+        choices: ["St1", "Circle K" ], 
+        nextScene: [3, 3,]
+    }, 
+
+    {   //Scene 3
+        description: "Bilen är tankad och klar. Nu raka vägen till jobbet. Du parkerar bilen och kollar upp mot kontoret. Går du in eller åker du hem till sängen igen? ",
+        choices: ["Går in", "Åker hem"], 
+        nextScene: [4, 5]
+    }, 
+    
+    { //Scene 4
+        description: "Du är nu på jobbet. Vill du dricka kaffe eller te? ",
+        choices: ["Kaffe", "Te"], 
+        nextScene: [6, 6]
+    }, 
+
+    { //Scene 5
+        description: "Skönt att vara hemma. Sätt på tvn och njut!",
+    
+    }, 
+    { //Scene 6
+        description: "Gott! Dags att jobba några timmar innan det är dags att åka hem igen. Ska det bli skönt att åka hem?",
+        choices: ["Ja", "Nej"], 
+        nextScene: [5, 5]  
+    },
+    { //Scene 7
+        description: "Kolla i lådan. Ok?",
+        choices: ['Ok'],
+        nextScene: [1] 
+    
+    }, 
+    { //Scene 8
+        description: "Dörren måste vara låst, annars kan någon gå in. Ok?",
+        choices: ['Ok'],
+        nextScene: [1] 
+    
     }, 
 ]; 
 
@@ -25,41 +58,35 @@ window.onload = presentScene;
 //Define the actions for our application 
 
 function presentScene() {
-    const answer = prompt(scenes[currentScene].description) 
+    const scene = scenes[currentScene];
+
+    const answer = prompt(scene.description) 
     handleUserChoice(answer)
 }
 
 function handleUserChoice(answer) {
     console.log(answer)
-     
 
-    if(answer === scenes[currentScene].choices[0]) {
-        currentScene = scenes[currentScene].nextScene [0]  
-    } else {
-        alert ('Kolla i lådan')
+    const scene = scenes[currentScene];
+
+    if (!scene.choices) {
+        alert('Slut')
+        return;
+    }
+    
+   //if (!scene.choices) {
+      //  alert('Kolla i lådan')
+    //}
+
+    if(answer === scene.choices[0]) { 
+        currentScene = scene.nextScene [0]  
     }
 
-    if(answer === scenes[currentScene].choices[1]) {
-        currentScene = scenes[currentScene].nextScene [1] 
+    if(answer === scene.choices[1]) {
+        currentScene = scene.nextScene [1] 
     } 
 
-    if(answer === scenes[currentScene].choices[2]) {
-      currentScene = scenes[currentScene].nextScene [2] 
-    } 
-    
-    if(answer === scenes[currentScene].choices[3]) {
-        currentScene = scenes[currentScene].nextScene [3] 
-
-    } 
     presentScene(); 
 }
 
-// Fråga David hur man avslutar. 
 
-/*
-function startCar () {
-    const button = document.getElementById("btn"); 
-    prompt
-
-}
-*/
